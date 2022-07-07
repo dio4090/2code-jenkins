21eccd11d44532bd58f11541b7ee77fe04e82675
Diogo Rocha <diogo.rocha42@outlook.com>
2022-07-07 16:34:34 -0300

    Ajuste no nome do node

--- src/br/com/twocode/Commands.groovy
+++ src/br/com/twocode/Commands.groovy
@@ -2,7 +2,7 @@ package br.com.twocode
 
 def exec(ProjectMetadata projectMetadata) {
     timeout(60) {
-        node ('mestre') {
+        node ('master') {
             try {
                 if (projectMetadata.projectName == 'finance-flutter-web') {
                     def webFlutter = new FinanceWebFlutter()
