8a6a24b2c33f2e4a054cb2b7036e292dbae6db80
Diogo Rocha <diogo.rocha42@outlook.com>
2022-07-07 16:44:39 -0300

    Adicionando classe

--- src/br/com/twocode/FinanceWebFlutter.groovy
+++ src/br/com/twocode/FinanceWebFlutter.groovy
@@ -1,48 +1,52 @@
 package br.com.twocode
 
-def exec(ProjectMetadata projectMetadata) {
-    //TODO: Quando tiver AWS trocar para a primeira linha
-    //projectMetadata.imageFullName = "${ECR_DEFAULT_REGISTRY}/${projectMetadata.projectName}:${BRANCH_NAME}-${BUILD_NUMBER}"
-    projectMetadata.imageFullName = "${projectMetadata.projectName}:${main}-${BUILD_NUMBER}"
+class FinanceWebFlutter {
 
-    try {
-        node ('docker_node') {
-            stage ('Git Checkout') {
-                gitPhase()
-            }
+    def exec(ProjectMetadata projectMetadata) {
+        //TODO: Quando tiver AWS trocar para a primeira linha
+        //projectMetadata.imageFullName = "${ECR_DEFAULT_REGISTRY}/${projectMetadata.projectName}:${BRANCH_NAME}-${BUILD_NUMBER}"
+        projectMetadata.imageFullName = "${projectMetadata.projectName}:${main}-${BUILD_NUMBER}"
 
-            stage ('Image Build Phase') {
-                node('docker_node') {
-                    imgBuildPhase(projectMetadata)
+        try {
+            node ('docker_node') {
+                stage ('Git Checkout') {
+                    gitPhase()
                 }
+
+                stage ('Image Build Phase') {
+                    node('docker_node') {
+                        imgBuildPhase(projectMetadata)
+                    }
+                }
+                stage ('Image Run Phase') {
+                        imgRunPhase(projectMetadata)
+                }
+                deleteDir()
             }
-            stage ('Image Run Phase') {
-                    imgRunPhase(projectMetadata)
-            }
+        } finally {
             deleteDir()
         }
-    } finally {
-        deleteDir()
     }
-}
 
-def gitPhase() {
-    checkout scm
-}
+    def gitPhase() {
+        checkout scm
+    }
 
-def imgBuildPhase(ProjectMetadata projectMetadata) {
-    gitPhase()
+    def imgBuildPhase(ProjectMetadata projectMetadata) {
+        gitPhase()
 
-    sh "docker build -t ${projectMetadata.imageFullName} \
-    --no-cache -f Dockerfile ."
-}
+        sh "docker build -t ${projectMetadata.imageFullName} \
+        --no-cache -f Dockerfile ."
+    }
 
-def imgRunPhase(ProjectMetadata projectMetadata) {
-    gitPhase()
+    def imgRunPhase(ProjectMetadata projectMetadata) {
+        gitPhase()
 
-    sh "echo DOCKER_IMAGE=${projectMetadata.imageFullName} >> .env"
-    sh "echo CONTAINER_NAME=${projectMetadata.projectName}-${BRANCH_NAME} >> .env"
+        sh "echo DOCKER_IMAGE=${projectMetadata.imageFullName} >> .env"
+        sh "echo CONTAINER_NAME=${projectMetadata.projectName}-${BRANCH_NAME} >> .env"
 
-    sh "docker image pull ${projectMetadata.imageFullName}"
-    sh "docker-compose -f docker-compose-ci.yaml -p ${projectMetadata.projectName}-${BRANCH_NAME} up -d"
+        sh "docker image pull ${projectMetadata.imageFullName}"
+        sh "docker-compose -f docker-compose-ci.yaml -p ${projectMetadata.projectName}-${BRANCH_NAME} up -d"
+    }
+
 }
