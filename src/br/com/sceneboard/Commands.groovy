c551f2acf516824858cd404b3477e63a4184fe01
Diogo Rocha <diogo.rocha42@outlook.com>
2022-07-07 16:19:34 -0300

    Adicionando Flutter Finance Web

--- src/br/com/sceneboard/Commands.groovy
+++ src/br/com/sceneboard/Commands.groovy
@@ -1,68 +1,18 @@
 package br.com.sceneboard
 
 def exec(ProjectMetadata projectMetadata) {
-
-    def status = false
-
     timeout(60) {
-
-        node ('master') {
-
+        node ('mestre') {
             try {
-
-                if(projectMetadata.projectName == "example") {
-                    def example = new Example()
-                    example.exec(projectMetadata)
+                if (projectMetadata.projectName == 'finance-flutter-web') {
+                    def webFlutter = new FinanceWebFlutter()
+                    webFlutter.exec(projectMetadata)
                 }
-
-                //status = true
-                //sendSlackMessage(status, projectMetadata.projectName)
-
             } catch (Exception e) {
-
-                //sendSlackMessage(status, projectMetadata.projectName)
                 throw e
-
             } finally {
-
                 deleteDir()
-            
             }
-
         }
-
     }
-
 }
-
-def sendSlackMessage(boolean status, String projectName) {
-
-    def color = ""
-    def title_text = ""
-    def attachment_text = "Veja mais detalhes abaixo"
-    def author_name = "Jenkins Webhook App"
-    //def webhook_url = "${SLACK_HOOK_URL}"
-    def content_type = "Content-type: application/json"
-    def message = ""
-    def messageRequest = ""
-    
-    if (status){
-        
-        color = "good"
-        title_text = "*Build Success! ${BUILD_TAG}*"
-        message = "{ \"text\": \"${title_text}\", \"attachments\": [ { \"text\": \"${attachment_text}\", \"author_name\":\"${author_name}\", \"color\": \"${color}\", \"actions\": [ { \"name\": \"Jenkins\", \"text\": \"Ver no Jenkins\", \"type\": \"button\", \"style\": \"primary\", \"url\": \"${BUILD_URL}console\" } ] } ] }"
-        //messageRequest = "curl -X POST -H '${content_type}' --data '${message}' ${webhook_url}"
-        sh "${messageRequest}"
-        
-    
-    } else {
-
-        color = "danger"
-        title_text = "*Build Failed! ${BUILD_TAG}*"
-        message = "{ \"text\": \"${title_text}\", \"attachments\": [ { \"text\": \"${attachment_text}\", \"author_name\":\"${author_name}\", \"color\": \"${color}\", \"actions\": [ { \"name\": \"Jenkins\", \"text\": \"Ver no Jenkins\", \"type\": \"button\", \"style\": \"danger\", \"url\": \"${BUILD_URL}console\" } ] } ] }"
-        //messageRequest = "curl -X POST -H '${content_type}' --data '${message}' ${webhook_url}"
-        sh "${messageRequest}"
-        
-    } 
-    
-} 
---
+++ src/br/com/sceneboard/FinanceWebFlutter.groovy
@@ -0,0 +1,46 @@
+package br.com.isi
+
+def exec(ProjectMetadata projectMetadata) {
+    projectMetadata.imageFullName = "${ECR_DEFAULT_REGISTRY}/${projectMetadata.projectName}:${BRANCH_NAME}-${BUILD_NUMBER}"
+
+    try {
+        node ('docker_node') {
+            stage ('Git Checkout') {
+                gitPhase()
+            }
+
+            stage ('Image Build Phase') {
+                node('docker_node') {
+                    imgBuildPhase(projectMetadata)
+                }
+            }
+            stage ('Image Run Phase') {
+                    imgRunPhase(projectMetadata)
+            }
+            deleteDir()
+        }
+    } finally {
+        deleteDir()
+    }
+}
+
+def gitPhase() {
+    checkout scm
+}
+
+def imgBuildPhase(ProjectMetadata projectMetadata) {
+    gitPhase()
+
+    sh "docker build -t ${projectMetadata.imageFullName} \
+    --no-cache -f Dockerfile ."
+}
+
+def imgRunPhase(ProjectMetadata projectMetadata) {
+    gitPhase()
+
+    sh "echo DOCKER_IMAGE=${projectMetadata.imageFullName} >> .env"
+    sh "echo CONTAINER_NAME=${projectMetadata.projectName}-${BRANCH_NAME} >> .env"
+
+    sh "docker image pull ${projectMetadata.imageFullName}"
+    sh "docker-compose -f docker-compose-ci.yaml -p ${projectMetadata.projectName}-${BRANCH_NAME} up -d"
+}
